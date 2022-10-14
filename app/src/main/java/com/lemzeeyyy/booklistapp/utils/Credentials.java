3b6681598c92f7de5926ebce89fe2210eb1fc3d9
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-10-14 17:10:32 +0100

    credentials

---
+++ app/src/main/java/com/lemzeeyyy/booklistapp/utils/Credentials.java
@@ -0,0 +1,6 @@
+package com.lemzeeyyy.booklistapp.utils;
+
+public class Credentials {
+    public static final String API_KEY = "AIzaSyC2tJSHNuXu0kAmiR7cAHv6hublPcOOsRg";
+    public static final String BASE_URL = "https://www.googleapis.com/";
+}
