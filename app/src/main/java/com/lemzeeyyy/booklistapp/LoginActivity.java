b55a5468b44df82eb19b7aa8d371dbc2898b29e9
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-10-18 23:14:37 +0100

    LoginActivity setup

--- app/src/main/java/com/lemzeeyyy/booklistapp/LoginActivity.java
+++ app/src/main/java/com/lemzeeyyy/booklistapp/LoginActivity.java
@@ -1,14 +1,97 @@
 package com.lemzeeyyy.booklistapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailEtLogin;
+    private AppCompatEditText passwordETLogin;
+    private Button loginBtn, createAcct;
+    private CheckBox rememberMe;
+    private TextView forgotPassword;
+    private FirebaseUser user;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        initializeWidgets();
+        createAcct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEtLogin.getText().toString().trim();
+                String password = Objects.requireNonNull(passwordETLogin.getText()).toString().trim();
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    emailEtLogin.setError("Email field cannot be empty");
+                    passwordETLogin.setError("Password field cannot be empty");
+                }else {
+                    signinUser(email,password);
+                }
+            }
+        });
+
+
+    }
+
+    private void signinUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(i);
+                            finish();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void initializeWidgets() {
+        emailEtLogin = findViewById(R.id.email_loginET);
+        passwordETLogin = findViewById(R.id.password_loginET);
+        loginBtn = findViewById(R.id.buttonLogin);
+        rememberMe = findViewById(R.id.rememberMe);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        createAcct = findViewById(R.id.createAcctLogin);
     }
 }
--- app/src/main/java/com/lemzeeyyy/booklistapp/RegisterActivity.java
+++ app/src/main/java/com/lemzeeyyy/booklistapp/RegisterActivity.java
@@ -3,12 +3,30 @@ package com.lemzeeyyy.booklistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText username, password, email;
+    private Button register;
+    private FirebaseAuth auth;
+    private DatabaseReference users;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initializeWidgets();
+    }
+
+    private void initializeWidgets() {
+        username = findViewById(R.id.usernameETRegister);
+        password = findViewById(R.id.passwordETRegister);
+        email = findViewById(R.id.emailETRegister);
+        register = findViewById(R.id.buttonRegister);
     }
 }
